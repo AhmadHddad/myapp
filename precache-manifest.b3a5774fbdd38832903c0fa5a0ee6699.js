5dd93a3b018badd1f2ecea0a4c345f9dbf615d87
AhmadHddad <a7mad-7dad@hotmail.com>
2019-09-27 21:17:03 +0300

    Updates

---
+++ precache-manifest.b3a5774fbdd38832903c0fa5a0ee6699.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199c1b3b7261a7c9b7e05d44c2669cdb",
+    "url": "/myApp/index.html"
+  },
+  {
+    "revision": "cab336aca7b1d08b9dd1",
+    "url": "/myApp/static/css/main.d8c61a38.chunk.css"
+  },
+  {
+    "revision": "6f19cafcf97ca3eac348",
+    "url": "/myApp/static/js/2.dfbf412a.chunk.js"
+  },
+  {
+    "revision": "cab336aca7b1d08b9dd1",
+    "url": "/myApp/static/js/main.d8ecae58.chunk.js"
+  },
+  {
+    "revision": "c66933264fcd14e3c945",
+    "url": "/myApp/static/js/runtime-main.6afef636.js"
+  },
+  {
+    "revision": "85c9f4fe39e7322e85b07908a32437e0",
+    "url": "/myApp/static/media/me.85c9f4fe.png"
+  }
+]);
